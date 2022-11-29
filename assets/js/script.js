c72c009e1667929ea62fa3884f7879e45e84aca6
YanDao0313 <3201176393@qq.com>
2022-11-29 15:06:34 +0800

    more!

--- assets/js/script.js
+++ assets/js/script.js
@@ -64,7 +64,7 @@ const musicData = [
   {
     backgroundImage: "https://xingqiu-tuchuang-1256524210.cos.ap-shanghai.myqcloud.com/5115/20221128221852.png",
     posterUrl: "https://xingqiu-tuchuang-1256524210.cos.ap-shanghai.myqcloud.com/5115/20221128221852.png",
-    title: "「聴きたかったダンスミュージック、リキッドルームに」 - サカナクション",
+    title: "「聴きたかったダンスミュージック、リキッドルームに」",
     album: "834.194",
     year: 2019,
     artist: "サカナクション",
@@ -96,6 +96,24 @@ const musicData = [
     year: 2014,
     artist: "サカナクション",
     musicPath: "https://xingqiu-tuchuang-1256524210.cos.ap-shanghai.myqcloud.com/5115/%E8%93%AE%E3%81%AE%E8%8A%B1%20-movie%20version-%20-%20%E3%82%B5%E3%82%AB%E3%83%8A%E3%82%AF%E3%82%B7%E3%83%A7%E3%83%B3.mp3",
+  },
+  {
+    backgroundImage: "https://xingqiu-tuchuang-1256524210.cos.ap-shanghai.myqcloud.com/5115/20221129150023.png",
+    posterUrl: "https://xingqiu-tuchuang-1256524210.cos.ap-shanghai.myqcloud.com/5115/20221129150023.png",
+    title: "僕と花",
+    album: "僕と花",
+    year: 2012,
+    artist: "サカナクション",
+    musicPath: "https://xingqiu-tuchuang-1256524210.cos.ap-shanghai.myqcloud.com/5115/%E5%83%95%E3%81%A8%E8%8A%B1%20-%20%E3%82%B5%E3%82%AB%E3%83%8A%E3%82%AF%E3%82%B7%E3%83%A7%E3%83%B3.flac",
+  },
+  {
+    backgroundImage: "https://xingqiu-tuchuang-1256524210.cos.ap-shanghai.myqcloud.com/5115/20221129150354.png",
+    posterUrl: "https://xingqiu-tuchuang-1256524210.cos.ap-shanghai.myqcloud.com/5115/20221129150354.png",
+    title: "三日月サンセット",
+    album: "GO TO THE FUTURE",
+    year: 2007,
+    artist: "サカナクション",
+    musicPath: "https://xingqiu-tuchuang-1256524210.cos.ap-shanghai.myqcloud.com/5115/%E4%B8%89%E6%97%A5%E6%9C%88%E3%82%B5%E3%83%B3%E3%82%BB%E3%83%83%E3%83%88%20-%20%E3%82%B5%E3%82%AB%E3%83%8A%E3%82%AF%E3%82%B7%E3%83%A7%E3%83%B3.flac",
   },
 ];
 
